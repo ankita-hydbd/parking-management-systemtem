5f8e062c995c41bc9675ae53cd0be9555c440192
kicktocode <ankita.hydbd@gmail.com>
2024-02-15 19:42:40 +0530

    Fix for compilation issue

--- src/main/java/com/example/parkingmanagementsystem/parking/spot/BatchParkingSpotRequest.java
+++ src/main/java/com/example/parkingmanagementsystem/parking/spot/BatchParkingSpotRequest.java
@@ -1,17 +1,11 @@
-////package com.example.parkingmanagementsystem.parking.spot;
-////
-////import jakarta.persistence.Entity;
-////import lombok.AllArgsConstructor;
-////import lombok.Data;
-////import lombok.NoArgsConstructor;
-////
-////import java.util.List;
-////
-////@Data
-////@AllArgsConstructor
-////@NoArgsConstructor
-////
-////public class BatchParkingSpotRequest {
-////
-//
-//}
+package com.example.parkingmanagementsystem.parking.spot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BatchParkingSpotRequest {
+
+
+}
